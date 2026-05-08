0187802dacc01e9e2fd207a7b57a8e4b6c8984d7
Cal-lifornia <76858909+Cal-lifornia@users.noreply.github.com>
2026-05-08 16:28:28 +1000

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.rs
@@ -0,0 +1,33 @@
+impl Solution {
+    pub fn sort_colors(nums: &mut Vec<i32>) {
+        let mut red = 0;
+        let mut white = 0;
+        let mut blue = 0;
+        nums.iter().for_each(|colour| {
+            eprintln!("colour: {colour}");
+            if *colour == 0 {
+                red += 1;
+            } else if *colour == 1 {
+                white += 1;
+            } else {
+                blue += 1;
+            }
+        });
+
+        white += red;
+        blue += white;
+
+        (0..red).for_each(|idx| {
+            eprintln!("idx: {idx}");
+            nums[idx] = 0;
+        });
+        (red..white).for_each(|idx| {
+            eprintln!("idx: {idx}");
+            nums[idx] = 1;
+        });
+        (white..blue).for_each(|idx| {
+            eprintln!("idx: {idx}");
+            nums[idx] = 2;
+        });
+    }
+}
