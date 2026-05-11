a4c6014a933cd65b162cc4b935c3d36a0debde74
Cal-lifornia <76858909+Cal-lifornia@users.noreply.github.com>
2026-05-11 13:34:36 +1000

    Add: custom-sort-string - submission-1

---
+++ Data Structures & Algorithms/custom-sort-string/submission-1.rs
@@ -0,0 +1,17 @@
+impl Solution {
+pub fn custom_sort_string(order: String, s: String) -> String {
+    let mut custom_order = [usize::MAX; 26];
+    order.bytes().enumerate().for_each(|(idx, ch)| {
+        let place = (ch - b'a') as usize;
+        custom_order[place] = idx + 1;
+    });
+
+    let mut s_bytes = s.bytes().collect::<Vec<u8>>();
+    s_bytes.sort_by(|a, b| {
+        let a_place = (a - b'a') as usize;
+        let b_place = (b - b'a') as usize;
+        custom_order[a_place].cmp(&custom_order[b_place])
+    });
+    String::from_utf8(s_bytes).unwrap()
+}
+}
