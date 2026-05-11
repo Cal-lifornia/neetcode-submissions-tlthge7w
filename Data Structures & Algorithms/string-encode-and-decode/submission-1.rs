cb0d9efb4054d68b4372d973d3ae84bc5c3671f3
Cal-lifornia <76858909+Cal-lifornia@users.noreply.github.com>
2026-05-11 14:53:49 +1000

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.rs
@@ -0,0 +1,26 @@
+impl Solution {
+    pub fn encode(strs: Vec<String>) -> String {
+        let mut encoded = String::new();
+        strs.into_iter().for_each(|item| {
+            let len = item.len();
+            encoded.push_str(format!("{len}\n{item}").as_str());
+        });
+        encoded
+    }
+
+    pub fn decode(s: String) -> Vec<String> {
+        let mut decoded = vec![];
+        let mut idx = 0;
+        while idx < s.len() {
+            let delimiter = s[idx..].find('\n').unwrap();
+            let len = s[idx..(idx + delimiter)].parse::<usize>().unwrap();
+            idx += delimiter + 1;
+            decoded.push(s[idx..(idx + len)].to_string());
+            idx += len;
+        }
+
+        decoded
+    }
+
+
+}
