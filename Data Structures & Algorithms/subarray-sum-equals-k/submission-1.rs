7691ba106b34a7dbfb606a3eeb1fb4d10ff4cc3c
Cal-lifornia <76858909+Cal-lifornia@users.noreply.github.com>
2026-05-12 16:58:34 +1000

    Add: subarray-sum-equals-k - submission-1

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-1.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn subarray_sum(nums: Vec<i32>, k: i32) -> i32 {
+        let mut count = 0;
+        let mut current_sum = 0;
+        let mut prefixes = HashMap::new();
+        prefixes.insert(0, 1);
+        for num in nums {
+            current_sum += num;
+            let diff = current_sum - k;
+            count += prefixes.get(&diff).unwrap_or(&0);
+            *prefixes.entry(current_sum).or_insert(0) += 1;
+        }
+        count
+    }
+}
