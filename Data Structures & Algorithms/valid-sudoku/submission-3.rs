5e8cdcd2727e9e87b43c5960e69dc595d6e7d904
Cal-lifornia <76858909+Cal-lifornia@users.noreply.github.com>
2026-05-12 13:12:38 +1000

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.rs
@@ -0,0 +1,27 @@
+impl Solution {
+    pub fn is_valid_sudoku(board: Vec<Vec<char>>) -> bool {
+        use std::collections::HashSet;
+        let mut squaresets = vec![vec![HashSet::<char>::new(); 3]; 3];
+        for y in 0..9 {
+            let mut rowset = HashSet::<char>::new();
+            let mut columnset = HashSet::<char>::new();
+            for x in 0..9 {
+                if board[y][x] != '.' {
+                    let squareset = &mut squaresets[y / 3][x / 3];
+                    if !squareset.insert(board[y][x]) {
+                        return false;
+                    }
+                    if !rowset.insert(board[y][x]) {
+                        return false;
+                    }
+                }
+                if board[x][y] != '.' && !columnset.insert(board[x][y]) {
+                    return false;
+                }
+            }
+            rowset.clear();
+            columnset.clear();
+        }
+        true
+    }
+}
