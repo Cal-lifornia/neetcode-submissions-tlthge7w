1bdaa9ab0a1a0fc2608d11079663168819779e99
Cal-lifornia <76858909+Cal-lifornia@users.noreply.github.com>
2026-05-19 13:22:47 +1000

    Add: dot-product-of-two-sparse-vectors - submission-0

---
+++ Data Structures & Algorithms/dot-product-of-two-sparse-vectors/submission-0.rs
@@ -0,0 +1,26 @@
+pub struct SparseVector {
+    nums: HashMap<usize, i32>,
+}
+
+impl SparseVector {
+    pub fn new(nums: Vec<i32>) -> Self {
+        let mut map = HashMap::<usize, i32>::new();
+        for (idx, num) in nums.into_iter().enumerate() {
+            if num > 0 {
+                map.insert(idx, num);
+            }
+        }
+        Self { nums: map }
+    }
+
+    pub fn dot_product(&self, vec: &SparseVector) -> i32 {
+        let mut product = 0;
+        for (key, &val) in &self.nums {
+            if let Some(&num) = vec.nums.get(key) {
+                product += val * num;
+            }
+        }
+        product
+    }
+}
+
